47bf7ef0b6525ec86ded9deef66dd2a78f7a787f
mrsteele <matt@omnionline.us>
2016-08-04 10:45:22 -0400

    feat: Adding support for safe dotenv compilation.
    closes #4

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import { parse } from 'dotenv'
+import dotenv from 'dotenv-safe'
 import fs from 'fs'
 import { DefinePlugin } from 'webpack'
 
 class Dotenv {
   constructor (options) {
-    options = options || {}
-    if (!options.path) options.path = './.env'
+    this.options = Object.assign({
+      path: './.env',
+      safe: false,
+      sample: './env.example'
+    }, options)
 
-    this.env = {}
+    this.blueprint = (this.safe) ? this.loadFile(this.options.sample) : this.loadFile(this.options.path)
+    this.env = this.loadFile(this.options.path)
+  }
+
+  loadFile (file) {
     try {
-      this.env = parse(fs.readFileSync(options.path))
-    } catch (err) {}
+      return dotenv.parse(fs.readFileSync(file))
+    } catch (err) {
+      return {}
+    }
   }
 
   apply (compiler) {
-    const plugin = Object.keys(this.env).reduce((definitions, key) => {
+    const plugin = Object.keys(this.blueprint).reduce((definitions, key) => {
       const value = process.env[key] || this.env[key]
+      if (!value) {
+        throw new Error(`Missing environment variable: ${key}`)
+      }
       definitions[`process.env.${key}`] = JSON.stringify(value)
       return definitions
     }, {})
