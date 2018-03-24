3663bba247c012a65eed9a2ec9db2eb310ebf047
Karol Majewski <info@karolmajewski.com>
2018-03-24 21:32:08 +0100

    chore: Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for dotenv-webpack
+// Project: https://github.com/mrsteele/dotenv-webpack
+// Definitions by: Karol Majewski <https://github.com/karol-majewski>
+// TypeScript Version: 2.7.2
+
+import * as webpack from 'webpack';
+
+declare class DotenvWebpackPlugin extends webpack.Plugin {
+  constructor(options?: DotenvWebpackPlugin.Options);
+}
+
+declare namespace DotenvWebpackPlugin {
+  interface Options {
+    /**
+     * The path to your environment variables. Default: `'./.env'`.
+     */
+    path?: string;
+
+    /**
+     * If `false` ignore safe-mode, if `true` load `'./.env.example'`, if a `string` load that file as the sample. Default: `false`.
+     */
+    safe?: boolean;
+
+    /**
+     * Set to `true` if you would rather load all system variables as well (useful for CI purposes). Default: `false`.
+     */
+    systemvars?: boolean;
+
+    /**
+     * If `true`, all warnings will be surpressed. Default: `false`.
+     */
+    silent?: boolean;
+  }
+}
+
+export = DotenvWebpackPlugin;
