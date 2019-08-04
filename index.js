7525b23f68fb12f9a0a69d876b7340dbed48c886
Emanual Jade <emanualjade@gmail.com>
2019-08-03 20:58:39 -0400

    chore(package): update babel to version ^7.5.5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   plugin = require('./dist').default
 } catch (err) {
   if (err.code === 'MODULE_NOT_FOUND') {
-    require('babel-register')
+    require('@babel/register')
     plugin = require('./src').default
   } else {
     console.log(err)
