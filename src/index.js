1e55cd619ad047c6dcbedfb37bd16c89449ee2e6
mrsteele <matt@omnionline.us>
2020-09-01 11:39:32 -0400

    feat: supporting webpack 5

--- src/index.js
+++ src/index.js
@@ -119,7 +119,7 @@ class Dotenv {
 
   formatData (vars = {}) {
     const { expand } = this.config
-    return Object.keys(vars).reduce((obj, key) => {
+    const formatted = Object.keys(vars).reduce((obj, key) => {
       const v = vars[key]
       const vKey = `process.env.${key}`
       let vValue
@@ -139,6 +139,12 @@ class Dotenv {
 
       return obj
     }, {})
+
+    // supporting webpack 5
+    formatted.process = '{"env": {}}'
+    formatted['process.env'] = '{}'
+
+    return formatted
   }
 
   /**
